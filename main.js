c3bef7d9ba4d91776675d0091ea9460d001f5228
YareniCruz <yarenicruz.050516@gmail.com>
2026-03-22 21:57:44 -0600

    Version 3: nudos de colores

--- main.js
+++ main.js
@@ -1,32 +1,62 @@
 import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
 
-// Crear la escena, cámara y renderizador
+// Escena
 const scene = new THREE.Scene();
+
+// Cámara
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Renderizador
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Crear geometría y material para el cubo
-const geometry = new THREE.BoxGeometry(1, 2, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0xff000f });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Luz
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(1, 1, 5);
+scene.add(light);
 
-// Agregar una luz direccional y ajustar su posición
-const directionalLight = new THREE.DirectionalLight(0xff0f0f, 1);
-directionalLight.position.set(1, 1, 5).normalize();
-scene.add(directionalLight);
 
-// Posicionar la cámara
-camera.position.z = 5;
+// Geometría   new THREE.IcosahedronGeometry(1);    // 20 caras
+const geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 16);
 
-// Función de animación con rotación y renderizado
+// Materiales
+const materialBlue = new THREE.MeshStandardMaterial({ color: 0x0000ff });
+const materialRed = new THREE.MeshStandardMaterial({ color: 0xc0c0c0 });
+const materialSilver = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+
+// Figuras
+const figure1 = new THREE.Mesh(geometry, materialBlue);
+const figure2 = new THREE.Mesh(geometry, materialRed);
+const figure3 = new THREE.Mesh(geometry, materialSilver);
+
+// Posiciones (línea horizontal)
+figure1.position.x = -3;
+figure2.position.x = 0;
+figure3.position.x = 3;
+
+// Velocidades
+figure1.userData.speed = 0.01;
+figure2.userData.speed = 0.04;
+figure3.userData.speed = 0.09;
+
+// Agregar a la escena
+scene.add(figure1, figure2, figure3);
+
+// Animación
 function animate() {
-    cube.rotation.x += 0.01; // Rotación en eje X
-    cube.rotation.y += 0.01; // Rotación en eje Y
+
+    figure1.rotation.x += figure1.userData.speed;
+    figure1.rotation.y += figure1.userData.speed;
+
+    figure2.rotation.x += figure2.userData.speed;
+    figure2.rotation.y += figure2.userData.speed;
+
+    figure3.rotation.x += figure3.userData.speed;
+    figure3.rotation.y += figure3.userData.speed;
+
     renderer.render(scene, camera);
 }
 
-// Iniciar la animación
 renderer.setAnimationLoop(animate);
